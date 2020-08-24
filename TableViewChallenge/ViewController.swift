7d86e1645c131e1fedbb2b8ea46172a0f4121ed2
Jitendra Kumar <Jitendra.kumar5@publicissapient.com>
2020-08-24 16:12:57 +0530

    Initial Commit

---
+++ TableViewChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewChallenge
+//
+//  Created by Jitendra Kumar on 24/08/20.
+//  Copyright © 2020 iOS developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
